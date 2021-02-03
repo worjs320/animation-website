b32cd687768159de36292aaf6ca21104bda41eb5
worjs320 <worjs320@naver.com>
2021-02-04 00:55:23 +0900

    feat: Work navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { menuData } from '../data/MenuData';
+
+const Nav = styled.nav`
+  height: 60px;
+  display: flex;
+  background: #000;
+`;
+
+const Logo = styled(Link)`
+  color: #fff;
+`;
+
+const MenuBars = styled.i``;
+
+const NavMenu = styled.div``;
+
+const NavMenuLinks = styled(Link)`
+  color: #fff;
+`;
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <Nav>
+      <Logo to="/">BARAK</Logo>
+      <MenuBars />
+      <NavMenu>
+        {menuData.map((item, index) => (
+          <NavMenuLinks to={item.link} key={index}>
+            {item.title}
+          </NavMenuLinks>
+        ))}
+      </NavMenu>
+    </Nav>
   );
 };
 
