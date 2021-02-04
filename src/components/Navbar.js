2fe8955a4d6be02d65e6baf6b833ee0f94b5f41c
worjs320 <worjs320@naver.com>
2021-02-05 00:07:35 +0900

    feat: Work Navbar's responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,76 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
 import { menuData } from '../data/MenuData';
+import { Button } from './Button';
+import Bars from '../images/bars.svg';
+// import { FaBars } from 'react-icons/fa';
 
 const Nav = styled.nav`
   height: 60px;
   display: flex;
-  background: #000;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  background: red;
+`;
+
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
 `;
 
 const Logo = styled(Link)`
-  color: #fff;
+  ${NavLink}
+  font-style:italic;
 `;
 
-const MenuBars = styled.i``;
+const MenuBars = styled.i`
+  display: none;
 
-const NavMenu = styled.div``;
+  @media screen and (max-width: 768px) {
+    display: block;
+    background-image: url(${Bars});
+    background-size: contain;
+    height: 40px;
+    width: 40px;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-50%, 25%);
+  }
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -48px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 
 const NavMenuLinks = styled(Link)`
-  color: #fff;
+  ${NavLink}
+`;
+
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const Navbar = () => {
@@ -33,6 +85,11 @@ const Navbar = () => {
           </NavMenuLinks>
         ))}
       </NavMenu>
+      <NavBtn>
+        <Button to="/content" primary="true">
+          Contanct Us
+        </Button>
+      </NavBtn>
     </Nav>
   );
 };
