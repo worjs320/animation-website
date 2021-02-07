e9b42275aea7f38fb6945678f7f6b761bc289525
worjs320 <worjs320@naver.com>
2021-02-07 20:27:32 +0900

    feat: Work HeroImage, HeroConetent

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ import { IoArrowForward, IoArrowBack } from 'react-icons/io5';
 
 const HeroSection = styled.section`
   height: 100vh;
-  max-width: 1100px;
+  max-height: 1100px;
   position: relative;
   overflow: hidden;
 `;
@@ -21,10 +21,72 @@ const HeroWrapper = styled.div`
   position: relative;
 `;
 
-const HeroSlide = styled.div``;
-const HeroSlider = styled.div``;
-const HeroImage = styled.div``;
-const HeroContent = styled.div``;
+const HeroSlide = styled.div`
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+`;
+
+const HeroSlider = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &::before {
+    content: '';
+    position: absolute;
+    z-index: 2;
+    width: 100%;
+    height: 100vh;
+    bottom: 0vh;
+    left: 0;
+    overflow: hidden;
+    opacity: 0.4;
+    background: linear-gradient(
+      0deg,
+      rgba(0, 0, 0, 0.2) 0%,
+      rgba(0, 0, 0, 0.2) 50%,
+      rgba(0, 0, 0, 0.6) 100%
+    );
+  }
+`;
+
+const HeroImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+`;
+const HeroContent = styled.div`
+  position: relative;
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  max-width: 1600px;
+  width: calc(100% - 100px);
+  color: #fff;
+
+  h1 {
+    font-size: clamp(1rem, 8vw, 2rem);
+    font-weight: 400;
+    text-transform: uppercase;
+    text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
+    text-align: left;
+    margin-bottom: 0.8rem;
+  }
+
+  p {
+    margin-bottom: 1.2rem;
+    text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
+  }
+`;
 const Arrow = styled(IoMdArrowRoundForward)``;
 
 const SliderButton = styled.div`
@@ -69,7 +131,7 @@ const Hero = ({ slides }) => {
           return (
             <HeroSlide key={index}>
               <HeroSlider>
-                <HeroImage />
+                <HeroImage src={slide.image} alt={slide.alt} />
                 <HeroContent>
                   <h1>{slide.title}</h1>
                   <p>{slide.price}</p>
