d422a733988bf48d509a4f327406571184eb65fc
worjs320 <worjs320@naver.com>
2021-02-11 23:21:10 +0900

    feat: Work image auto slide function, add image animation effect

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import styled, { css } from 'styled-components/macro';
+import styled, { css, keyframes } from 'styled-components/macro';
 import { Button } from './Button';
 import { IoMdArrowRoundForward } from 'react-icons/io';
 import { IoArrowForward, IoArrowBack } from 'react-icons/io5';
@@ -56,6 +56,16 @@ const HeroSlider = styled.div`
   }
 `;
 
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
 const HeroImage = styled.img`
   position: absolute;
   top: 0;
@@ -63,7 +73,9 @@ const HeroImage = styled.img`
   width: 100vw;
   height: 100vh;
   object-fit: cover;
+  animation: ${fadeIn} 2s;
 `;
+
 const HeroContent = styled.div`
   position: relative;
   z-index: 10;
@@ -130,19 +142,17 @@ const Hero = ({ slides }) => {
   const length = slides.length;
   const timeout = useRef(null);
 
-  // useEffect(() => {
-  //   const nextSlide = () => {
-  //     setCurrent((current) => (current === length - 1 ? 0 : current + 1));
-  //   };
+  useEffect(() => {
+    const nextSlide = () => {
+      setCurrent((current) => (current === length - 1 ? 0 : current + 1));
+    };
 
-  //   timeout.current = setTimeout(nextSlide, 3000);
+    timeout.current = setTimeout(nextSlide, 3000);
 
-  //   return function () {
-  //     if (timeout.currnet) {
-  //       clearTimeout(timeout.current);
-  //     }
-  //   };
-  // }, [current, length]);
+    return function () {
+      clearTimeout(timeout.current);
+    };
+  }, [current, length]);
 
   const nextSlide = () => {
     setCurrent(current === length - 1 ? 0 : current + 1);
